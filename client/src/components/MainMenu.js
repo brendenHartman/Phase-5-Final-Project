3d689947c802018086356e56481d7fbe0b0e54d5
Brenden Hartman <brendenhart22@gmail.com>
2025-01-03 13:41:56 -0600

    css workday 1

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -6,7 +6,7 @@ function MainMenu(){
     const dispatch = useDispatch()
     const user = useSelector((state) => state.user)
     let userSec = 
-    (<div id='userSec'>
+    (<div id='userSecLog'>
         <Link id='logInLink' className="logInLink" to="/login">Login</Link>
         <h1>/</h1>
         <Link id='signUpLink' className='signUpLink' to='/signup'>Signup</Link>
@@ -15,7 +15,12 @@ function MainMenu(){
     let playButton = <Link className='playButt' to='/Login'>Play</Link>
 
     if('id' in user){
-      userSec = <h1 id='' >Welcome, {user.username}!</h1>
+      userSec = (
+        <div id="userSecIn">
+            <h1 id='welcome' >Welcome,</h1>
+            <h1 id="username">{user.username}!</h1>
+            <button id='signOutButton' onClick={handleSignOut}>Sign-Out</button>
+        </div>)
       playButton = <Link className='playButt' to='/Zoo'>Play</Link>
     }
 
@@ -33,9 +38,7 @@ function MainMenu(){
             <h1 id='mainTitle'>ZooOlogy</h1>
             {playButton}
             <Link id='tutLink' to='/tutorial'>Tutorial</Link>
-            <div id='MainMenuDivider'></div>
             {userSec}
-            <button id='signOutButton' onClick={handleSignOut}>SignOut</button>
         </div>
     )
 }
